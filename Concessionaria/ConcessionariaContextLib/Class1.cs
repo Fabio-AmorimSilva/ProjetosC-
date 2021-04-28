4b28a5acb97ef25992221c55cd2bb7e1456d232d
Fabio <fabioamorim2025@hotmail.com>
2021-04-28 17:49:07 -0300

    Adicionandos arquivos da Concessionaria Entidades e Contexto do banco de dados

---
+++ Concessionaria/ConcessionariaContextLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ConcessionariaContextLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ Concessionaria/ConcessionariaEntitiesLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ConcessionariaEntitiesLib
+{
+    public class Class1
+    {
+    }
+}
