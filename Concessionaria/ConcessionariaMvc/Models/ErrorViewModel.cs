d3802852509af061b9b4a0a2ee9124ac4d2c67c2
Fabio <fabioamorim2025@hotmail.com>
2021-05-03 15:07:57 -0300

    Adicionando ConcessionariaMvc e React

---
+++ Concessionaria/ConcessionariaMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ConcessionariaMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
