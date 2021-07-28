5c04145b65d619d09ffea50c15994615c74bb966
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2021-07-28 17:25:48 -0300

    Correção de código

--- Imobiliaria/ImobiliariaEntities/Imovel.cs
+++ Imobiliaria/ImobiliariaEntities/Imovel.cs
@@ -1,7 +1,18 @@
+using System;
+
 namespace ImobiliariaEntities
 {
     public class Imovel
     {
         
+        public int id;
+
+        public int idDono;
+
+        public int idAgencia;
+
+        public string endereco;
+
+        public double preco;
     }
 }
