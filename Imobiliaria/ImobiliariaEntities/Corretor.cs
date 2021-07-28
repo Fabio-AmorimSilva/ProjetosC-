7eee42f4182502e3044f9d7a1a787885d08af6a0
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2021-07-28 17:25:39 -0300

    Correção de código

--- Imobiliaria/ImobiliariaEntities/Corretor.cs
+++ Imobiliaria/ImobiliariaEntities/Corretor.cs
@@ -1,7 +1,20 @@
+using System;
+
 namespace ImobiliariaEntities
 {
     public class Corretor
     {
         
+        public int id;
+
+        public int idAgencia;
+
+        public string nome;
+
+        public int idade;
+
+        public int vendas;
+
+
     }
 }
