facee6c31e0092e20cd3c8f1fea778dcb29735b3
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2021-07-30 17:17:10 -0300

    Adicionando controlador de Dono

--- Imobiliaria/ImobiliariaMvc/Controllers/DonoController.cs
+++ Imobiliaria/ImobiliariaMvc/Controllers/DonoController.cs
@@ -5,6 +5,17 @@ namespace ImobiliariaMvc.Controllers
 {
     public class DonoController : Controller
     {
-        
+        private readonly ILogger<DonoController> _logger;
+
+        public DonoController(ILogger<DonoController> logger){
+
+            _logger = logger;
+
+        }
+
+        public IActionResult Donos(){
+            return View();
+            
+        }
     }
 }
