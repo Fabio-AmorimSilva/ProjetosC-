3ea26f8a3e0cfc0509fe86aa35efd2a8aa25d2c9
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2021-07-31 18:07:43 -0300

    Tabelas adicionadas e dados incompletos

--- Imobiliaria/Imobiliaria.sql
+++ Imobiliaria/Imobiliaria.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.3.3 on qua jul 28 17:14:19 2021
+-- File generated with SQLiteStudio v3.3.3 on sáb jul 31 18:06:32 2021
 --
 -- Text encoding used: System
 --
@@ -9,6 +9,15 @@ BEGIN TRANSACTION;
 -- Table: Agencia
 CREATE TABLE Agencia (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR (40) NOT NULL UNIQUE, cidade VARCHAR (40) UNIQUE NOT NULL, idCorretores INTEGER UNIQUE NOT NULL, idImoveis INTEGER UNIQUE NOT NULL);
 INSERT INTO Agencia (id, nome, cidade, idCorretores, idImoveis) VALUES (1, 'Imoveis Jandira', 'Jandira', '1,2,3', '1,2,3');
+
+-- Table: Corretor
+CREATE TABLE Corretor (id INTEGER PRIMARY KEY NOT NULL UNIQUE, idAgencia INTEGER NOT NULL UNIQUE, nome VARCHAR NOT NULL, idade INTEGER NOT NULL, vendas INTEGER NOT NULL);
+
+-- Table: Dono
+CREATE TABLE Dono (cpf INTEGER PRIMARY KEY UNIQUE NOT NULL, nome VARCHAR UNIQUE NOT NULL, idade INTEGER NOT NULL, imovel VARCHAR NOT NULL);
+
+-- Table: Imovel
+CREATE TABLE Imovel (id INTEGER PRIMARY KEY NOT NULL UNIQUE, idDono INTEGER UNIQUE NOT NULL, idAgencia INTEGER UNIQUE NOT NULL, endereco VARCHAR UNIQUE NOT NULL, preco DOUBLE UNIQUE NOT NULL);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
