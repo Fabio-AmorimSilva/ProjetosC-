53439e493442aa51633192ce96c03e7c628f8b0a
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2021-08-03 18:11:11 -0300

    Método Get para listar agências

--- Imobiliaria/ImobiliariaMvc/Controllers/AgenciaController.cs
+++ Imobiliaria/ImobiliariaMvc/Controllers/AgenciaController.cs
@@ -1,3 +1,6 @@
+using System.Linq;
+using ImobiliariaContext;
+using ImobiliariaMvc.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -6,16 +9,26 @@ namespace ImobiliariaMvc.Controllers
     public class AgenciaController : Controller
     {
         
+        private Imobiliaria db;
+
         private readonly ILogger<AgenciaController> _logger;
 
-        public AgenciaController(ILogger<AgenciaController> logger){
+        public AgenciaController(ILogger<AgenciaController> logger, Imobiliaria injectedContext){
 
             _logger = logger;
+            db = injectedContext;
 
         }
 
         public IActionResult Agencias(){
-            return View();
+
+            var model = new AgenciaViewModel{
+                Agencias = db.Agencias.ToList()
+
+            };
+           
+
+            return View(model);
 
         }
     }
