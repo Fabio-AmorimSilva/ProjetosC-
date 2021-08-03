a21705e609f1188653e775066cae5e65155d56e3
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2021-08-03 18:10:08 -0300

    Adicionando modelo para dados das Agencias

---
+++ Imobiliaria/ImobiliariaMvc/Models/AgenciaViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using ImobiliariaEntities;
+
+namespace ImobiliariaMvc.Models
+{
+    public class AgenciaViewModel
+    {
+        public IEnumerable<Agencia> Agencias{get; set;}
+    }
+}
