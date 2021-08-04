f5e077e9b8b355539de19c8930792097cb2ed14e
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2021-08-04 11:32:58 -0300

    Atualização de banco de dados

--- Imobiliaria/Imobiliaria.sql
+++ Imobiliaria/Imobiliaria.sql
@@ -13,26 +13,26 @@ INSERT INTO Agencias (id, nome, cidade, idCorretores, idImoveis) VALUES (3, 'Imo
 INSERT INTO Agencias (id, nome, cidade, idCorretores, idImoveis) VALUES (4, 'Imoveis Osasco', 'Osasco', '10,11,12', '10,11,12');
 
 -- Table: Corretor
-DROP TABLE IF EXISTS Corretor;
-CREATE TABLE Corretor (id INTEGER PRIMARY KEY NOT NULL UNIQUE, idAgencia INTEGER NOT NULL UNIQUE, nome VARCHAR NOT NULL, idade INTEGER NOT NULL, vendas INTEGER NOT NULL);
-INSERT INTO Corretor (id, idAgencia, nome, idade, vendas) VALUES (1, 1, 'Fabio', 26, 10);
-INSERT INTO Corretor (id, idAgencia, nome, idade, vendas) VALUES (2, 2, 'Joao', 30, 20);
-INSERT INTO Corretor (id, idAgencia, nome, idade, vendas) VALUES (3, 3, 'Maria', 31, 30);
-INSERT INTO Corretor (id, idAgencia, nome, idade, vendas) VALUES (4, 4, 'Joana', 20, 5);
+DROP TABLE IF EXISTS Corretores;
+CREATE TABLE Corretores (id INTEGER PRIMARY KEY NOT NULL UNIQUE, idAgencia INTEGER NOT NULL UNIQUE, nome VARCHAR NOT NULL, idade INTEGER NOT NULL, vendas INTEGER NOT NULL);
+INSERT INTO Corretores (id, idAgencia, nome, idade, vendas) VALUES (1, 1, 'Fabio', 26, 10);
+INSERT INTO Corretores (id, idAgencia, nome, idade, vendas) VALUES (2, 2, 'Joao', 30, 20);
+INSERT INTO Corretores (id, idAgencia, nome, idade, vendas) VALUES (3, 3, 'Maria', 31, 30);
+INSERT INTO Corretores (id, idAgencia, nome, idade, vendas) VALUES (4, 4, 'Joana', 20, 5);
 
 -- Table: Dono
-DROP TABLE IF EXISTS Dono;
-CREATE TABLE Dono (cpf INTEGER PRIMARY KEY UNIQUE NOT NULL, nome VARCHAR UNIQUE NOT NULL, idade INTEGER NOT NULL, imovel VARCHAR NOT NULL);
-INSERT INTO Dono (cpf, nome, idade, imovel) VALUES (1, 'Mariana', 20, 'Jandira');
-INSERT INTO Dono (cpf, nome, idade, imovel) VALUES (2, 'Paulo', 21, 'Barueri');
-INSERT INTO Dono (cpf, nome, idade, imovel) VALUES (3, 'Juliana', 25, 'Carapicuiba');
-INSERT INTO Dono (cpf, nome, idade, imovel) VALUES (4, 'Lucas', 30, 'Osasco');
+DROP TABLE IF EXISTS Donos;
+CREATE TABLE Donos (id INTEGER PRIMARY KEY UNIQUE NOT NULL, nome VARCHAR UNIQUE NOT NULL, idade INTEGER NOT NULL, imovel VARCHAR NOT NULL);
+INSERT INTO Donos (id, nome, idade, imovel) VALUES (1, 'Mariana', 20, 'Jandira');
+INSERT INTO Donos (id, nome, idade, imovel) VALUES (2, 'Paulo', 21, 'Barueri');
+INSERT INTO Donos (id, nome, idade, imovel) VALUES (3, 'Juliana', 25, 'Carapicuiba');
+INSERT INTO Donos (id, nome, idade, imovel) VALUES (4, 'Lucas', 30, 'Osasco');
 
 -- Table: Imovel
-DROP TABLE IF EXISTS Imovel;
-CREATE TABLE Imovel (id INTEGER PRIMARY KEY NOT NULL UNIQUE, idDono INTEGER UNIQUE NOT NULL, idAgencia INTEGER UNIQUE NOT NULL, endereco VARCHAR UNIQUE NOT NULL, preco DOUBLE UNIQUE NOT NULL);
-INSERT INTO Imovel (id, idDono, idAgencia, endereco, preco) VALUES (1, 1, 1, 'Jandira', 100.0);
-INSERT INTO Imovel (id, idDono, idAgencia, endereco, preco) VALUES (2, 2, 2, 'Barueri', 200.0);
-INSERT INTO Imovel (id, idDono, idAgencia, endereco, preco) VALUES (3, 3, 3, 'Carapicuiba', 80.0);
-INSERT INTO Imovel (id, idDono, idAgencia, endereco, preco) VALUES (4, 4, 4, 'Osasco', 250.0);
+DROP TABLE IF EXISTS Imoveis;
+CREATE TABLE Imoveis (id INTEGER PRIMARY KEY NOT NULL UNIQUE, idDono INTEGER UNIQUE NOT NULL, idAgencia INTEGER UNIQUE NOT NULL, endereco VARCHAR UNIQUE NOT NULL, preco DOUBLE UNIQUE NOT NULL);
+INSERT INTO Imoveis (id, idDono, idAgencia, endereco, preco) VALUES (1, 1, 1, 'Jandira', 100.0);
+INSERT INTO Imoveis (id, idDono, idAgencia, endereco, preco) VALUES (2, 2, 2, 'Barueri', 200.0);
+INSERT INTO Imoveis (id, idDono, idAgencia, endereco, preco) VALUES (3, 3, 3, 'Carapicuiba', 80.0);
+INSERT INTO Imoveis (id, idDono, idAgencia, endereco, preco) VALUES (4, 4, 4, 'Osasco', 250.0);
 
