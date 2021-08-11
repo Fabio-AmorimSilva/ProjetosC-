70600bfb869dee6601d20abfc0220afa06727f3a
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2021-08-11 17:05:22 -0300

    Adicionando método GET da API Agência

--- Imobiliaria/ImobiliariaMvc/Controllers/HomeController.cs
+++ Imobiliaria/ImobiliariaMvc/Controllers/HomeController.cs
@@ -6,16 +6,49 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ImobiliariaMvc.Models;
+using System.Net.Http;
+using ImobiliariaContext;
+using ImobiliariaEntities;
+using System.Net.Http.Json;
 
 namespace ImobiliariaMvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory clientFactory;
 
-        public HomeController(ILogger<HomeController> logger)
+        private Imobiliaria db;
+
+        public HomeController(
+            ILogger<HomeController> logger, 
+            Imobiliaria injectedContext,
+            IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
+            db = injectedContext;
+            clientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Agencias(){
+
+            string uri;
+            uri = "api/Agencias";
+
+            var client = clientFactory.CreateClient(
+              name: "ImobiliariaService"  
+            );
+
+            var request = new HttpRequestMessage(
+                method: HttpMethod.Get, requestUri: uri
+            );
+
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            var model = await response.Content.ReadFromJsonAsync<IEnumerable<Agencia>>();
+
+            return View(model);
+
         }
 
         public IActionResult Index()
