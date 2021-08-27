974fb8383bd12e29086b5c31d6417f7bf5fbf022
Fabio-AmorimSilva <fabioamorim2025@hotmail.com>
2021-08-27 10:52:08 -0300

    Adicionando pastas e arquivos com scalffoding para acesso as funcionalidades da Identity

---
+++ Login/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Login.Areas.Identity.Data;
+using Login.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Login.Areas.Identity.IdentityHostingStartup))]
+namespace Login.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<LoginContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("LoginContextConnection")));
+
+                services.AddDefaultIdentity<LoginUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<LoginContext>();
+            });
+        }
+    }
+}
