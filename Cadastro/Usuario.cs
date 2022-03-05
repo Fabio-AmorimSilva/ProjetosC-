827654a34716321949a6a9738fcade9477c93b37
Fabio-AmorimSilva <Fabio-AmorimSilva>
2022-03-05 18:43:41 -0300

    Adicionando classe Usuário

---
+++ Cadastro/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro
+{
+    public class Usuario
+    {
+
+        public string Nome { get; set; }
+        public int Cpf { get; set; }
+        public int Idade { get; set; }
+        public string Cidade { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {this.Nome} "
+                   +$"CPF: {this.Cpf}"
+                   +$"Idade: {this.Idade}"
+                   +$"Cidade: {this.Cidade}";
+
+        }
+
+
+    }
+}
