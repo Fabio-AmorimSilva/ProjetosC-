48c4411ec059867e3a49e6240f04324ac62d1f31
Fabio-AmorimSilva <Fabio-AmorimSilva>
2022-03-06 15:58:08 -0300

    Testes de dados de usuário adicionados

--- TestesCadastro/UsuarioTestes.cs
+++ TestesCadastro/UsuarioTestes.cs
@@ -42,5 +42,49 @@ namespace TestesCadastro
             Assert.Equal(123, usuario.Cpf);
 
         }
+
+        [Fact(DisplayName = "Testa de criação de Usuário")]
+        public void TesteUsuario()
+        {
+
+            //Arrange
+            usuario.Nome = "Fabio";
+            usuario.Cpf = 123;
+            usuario.Idade = 26;
+            usuario.Cidade = "São Paulo";
+
+            //Act
+            string dados = usuario.ToString();
+
+            //Assert
+            Assert.Contains("Dados do Usuário", dados);
+
+        }
+
+        [Theory]
+        [InlineData("Fabio", 123, 26, "São Paulo")]
+        [InlineData("Pedro", 456, 30, "Osasco")]
+        [InlineData("João", 789, 40, "Barueri")]
+        [InlineData("Marcos", 101112, 50, "Itapevi")]
+        public void TestaDadosDeUsuarios(string nome,
+                                         int cpf,
+                                         int idade,
+                                         string cidade)
+        {
+
+            //Arrange
+            usuario.Nome = nome;
+            usuario.Cpf = cpf;
+            usuario.Idade = idade;
+            usuario.Cidade = cidade;
+
+            //Act
+            string dados = usuario.ToString();
+
+            //Assert
+            Assert.Equal(nome, usuario.Nome);
+
+        }
+
     }
 }
