23a44034d70ba7a78e161ee5b3063c279ab127ac
fabio <fabioamorin120@gmail.com>
2023-06-11 15:46:51 -0300

    feat: Creating context class

---
+++ Library/Library.Infrastructure/LibraryContext.cs
@@ -0,0 +1,17 @@
+﻿using Library.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Infrastructure;
+
+public class LibraryContext : DbContext
+{
+    public DbSet<Author> Authors {  get; set; }
+    public DbSet<Book> Books {  get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<LibraryUnit> Libraries { get; set; }
+
+    public LibraryContext(DbContextOptions<LibraryContext> options): base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        => modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+}
