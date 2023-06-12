5641b1017eeee3177b442c626621b82a98311333
fabio <fabioamorin120@gmail.com>
2023-06-12 19:56:59 -0300

    feat: Adding new service and packages

---
+++ Library/Library.Application/Services/TokenService.cs
@@ -0,0 +1,37 @@
+﻿using Library.Application.Extensions;
+using Library.Application.Options;
+using Library.Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Library.Application.Services;
+
+public class TokenService
+{
+    private readonly Settings _settings;
+
+    public TokenService(IOptions<Settings> settings)
+    {
+        _settings = settings.Value;
+    }
+
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_settings.JwtKey);
+        var claims = user.GetClaims();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddHours(8),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
