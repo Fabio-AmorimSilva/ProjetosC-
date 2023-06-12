9199a8a80f146c1063cbe640c9e8b146022b5052
fabio <fabioamorin120@gmail.com>
2023-06-12 19:56:07 -0300

    feat: Adding new viewmodels

---
+++ Library/Library.Application/ViewModels/User/RegisterViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.Application.ViewModels.User;
+
+public struct RegisterViewModel
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+}
