4190a1bcf6955d6738616788cacea998f90ec281
fabio <fabioamorin120@gmail.com>
2023-06-12 19:56:24 -0300

    feat: Adding new extesion

---
+++ Library/Library.Application/Extensions/RoleClaimsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Library.Domain.Entities;
+using System.Security.Claims;
+
+namespace Library.Application.Extensions;
+
+public static class RoleClaimsExtensions
+{
+    public static List<Claim> GetClaims(this User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Sid, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Email),
+            new Claim(ClaimTypes.Role, user.Role.ToString()),
+        };
+
+        return claims;
+    }
+}
