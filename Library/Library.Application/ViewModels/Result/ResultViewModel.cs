1e3d4fdc7a775fdec4a674223773b6b36e197be4
fabio <fabioamorin120@gmail.com>
2023-06-14 19:50:25 -0300

    feat: Creating new class to padronize responses

---
+++ Library/Library.Application/ViewModels/Result/ResultViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Library.Application.ViewModels.Result;
+
+public class ResultViewModel<T>
+{
+    public T Data { get; init; }
+    public List<string>? Errors { get; init; }
+
+    public ResultViewModel(T data, List<string> errors)
+    {
+        Data = data;
+        Errors = errors;
+    }
+
+    public ResultViewModel(T data)
+        => Data = data;
+
+    public ResultViewModel(List<string> errors)
+        => Errors = errors;
+
+    public ResultViewModel(string error)
+        => Errors.Add(error);
+
+}
