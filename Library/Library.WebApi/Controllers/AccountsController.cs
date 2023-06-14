194ac2e7482879b9bfaa297b65e3ad70662c0e69
fabio <fabioamorin120@gmail.com>
2023-06-14 19:50:48 -0300

    feat: Creating register endpoint

--- Library/Library.WebApi/Controllers/AccountsController.cs
+++ Library/Library.WebApi/Controllers/AccountsController.cs
@@ -1,10 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Library.Application.ViewModels.Result;
+using Library.Application.ViewModels.User;
+using Library.Domain.Entities;
+using Library.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Library.WebApi.Controllers;
 
 [ApiController]
 public class AccountsController : ControllerBase
 {
-    [HttpPost]
-    public IActionResult Login([FromBody] ) 
+
+    private readonly LibraryContext _context;
+
+    public AccountsController(LibraryContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("v1/accounts/signup")]
+    public async Task<IActionResult> Login([FromBody] RegisterViewModel register)
+    {
+        var userExists = await _context
+            .Users
+            .FirstOrDefaultAsync(u => u.Email == register.Email, cancellationToken:default);
+
+        if(userExists is not null)
+            return BadRequest(new ResultViewModel<User>("Email alerady used"));
+
+
+        var user = new User(
+            name: register.Name,
+            email: register.Email,
+            password: register.Password,
+            role: register.Role);
+
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+
+        return Created($"{user.Name}", user);
+    } 
 }
