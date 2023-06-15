64e83e1f446233a5f09f08c2114bcd55b9651361
fabio <fabioamorin120@gmail.com>
2023-06-15 20:02:11 -0300

    feat: Adding login endpoint

--- Library/Library.WebApi/Controllers/AccountsController.cs
+++ Library/Library.WebApi/Controllers/AccountsController.cs
@@ -1,4 +1,6 @@
-﻿using Library.Application.ViewModels.Result;
+﻿using Library.Application.Services;
+using Library.Application.ViewModels.Login;
+using Library.Application.ViewModels.Result;
 using Library.Application.ViewModels.User;
 using Library.Domain.Entities;
 using Library.Infrastructure;
@@ -12,14 +14,18 @@ public class AccountsController : ControllerBase
 {
 
     private readonly LibraryContext _context;
+    private readonly TokenService _tokenService;
 
-    public AccountsController(LibraryContext context)
+    public AccountsController(
+        LibraryContext context,
+        TokenService service)
     {
         _context = context;
+        _tokenService = service;
     }
 
     [HttpPost("v1/accounts/signup")]
-    public async Task<IActionResult> Login([FromBody] RegisterViewModel register)
+    public async Task<IActionResult> Signup([FromBody] RegisterViewModel register)
     {
         var userExists = await _context
             .Users
@@ -39,5 +45,20 @@ public class AccountsController : ControllerBase
         await _context.SaveChangesAsync();
 
         return Created($"{user.Name}", user);
-    } 
+    }
+
+    [HttpPost("v1/accounts/login")]
+    public async Task<IActionResult> Login([FromBody] LoginViewModel login)
+    {
+        var userExists = await _context
+            .Users
+            .FirstOrDefaultAsync(u => u.Email == login.Username && u.Password == login.Password, cancellationToken: default);
+
+        if(userExists is null)
+            return NotFound(new ResultViewModel<User>("User name or password are incorrect"));
+
+        var token = _tokenService.GenerateToken(userExists);
+
+        return Ok(new { Token = token });
+    }
 }
