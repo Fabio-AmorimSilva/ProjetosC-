ef8eb7719d82b9dbd6a302a9d12249b522b58f73
fabio <fabioamorin120@gmail.com>
2023-06-17 17:34:18 -0300

    feat: Creating new viewmodels

---
+++ Library/Library.Application/ViewModels/Authors/AuthorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Library.Domain.Entities;
+
+namespace Library.Application.ViewModels.Authors;
+
+public struct AuthorViewModel
+{
+    public string Name { get; init; }
+    public string Country { get; init; }
+    public DateTime Birth { get; init; }
+    public IEnumerable<Book> Books { get; init; }
+}
---
+++ Library/Library.Application/ViewModels/Library/LibraryUnitViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Library.Domain.Entities;
+
+namespace Library.Application.ViewModels.Library;
+
+public struct LibraryUnitViewModel
+{
+    public string Name { get; init; }
+    public string City { get; init; }
+    public List<Book> Books { get; init; }
+}
