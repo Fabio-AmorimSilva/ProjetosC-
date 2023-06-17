cabc2f3a17f4856d384e82f6f5960474b5fdcca1
fabio <fabioamorin120@gmail.com>
2023-06-17 17:34:48 -0300

    feat: Creating new configuration for authentication

---
+++ Library/Library.WebApi/Configuration/ConfigureAuthentication.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Library.WebApi.Configuration;
+
+public static class ConfigureAuthentication
+{
+    public static IServiceCollection AuthenticationConfig(this IServiceCollection services, IConfiguration configuration)
+    {
+        var settings = configuration.GetValue<string>("JwtKey");
+        var key = Encoding.ASCII.GetBytes(settings);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidateAudience = true
+            };
+        });
+
+        return services;
+    }
+}
