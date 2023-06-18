5d99a27cea8b03d1bc32d10e18b4e88fe3a771a4
fabio <fabioamorin120@gmail.com>
2023-06-18 14:14:14 -0300

    feat: Adding new services and code formatting

--- Library/Library.WebApi/Program.cs
+++ Library/Library.WebApi/Program.cs
@@ -3,20 +3,21 @@ using Library.Application.Services;
 using Library.Infrastructure;
 using Library.WebApi.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<LibraryContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseSqlServer(connectionString);
-});
 
-builder.Services.AuthenticationConfig(builder.Configuration);
+ConfigureDbContext(builder);
 ConfigureServices(builder);
+ConfigureJwt(builder);
+ConfigureJsonOptions(builder);
 
-builder.Services.AddControllers();
+//External Configs
+builder.Services.AuthenticationConfig(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -30,7 +31,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+      .AllowAnyOrigin()
+      .AllowAnyMethod()
+      .AllowAnyHeader());
+
 app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseAuthentication();
 
@@ -40,9 +48,61 @@ app.MapControllers();
 
 app.Run();
 
-
 void ConfigureServices(WebApplicationBuilder builder) 
 {
     builder.Services.AddTransient<TokenService>();
     builder.Services.Configure<Settings>(builder.Configuration);
+}
+
+void ConfigureJwt(WebApplicationBuilder builder)
+{
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using Bearer scheme.",
+        });
+
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header
+                },
+                new List<string>()
+            }
+        });
+    });
+}
+
+void ConfigureDbContext(WebApplicationBuilder builder)
+{
+    builder.Services.AddDbContext<LibraryContext>(options =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        options.UseSqlServer(connectionString);
+    });
+}
+
+void ConfigureJsonOptions(WebApplicationBuilder builder)
+{
+    builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
 }
