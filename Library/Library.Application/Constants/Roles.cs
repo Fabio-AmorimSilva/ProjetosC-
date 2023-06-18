a3a9c761a15464ed835da96d4530fc1fd9fdae9d
fabio <fabioamorin120@gmail.com>
2023-06-18 14:10:36 -0300

    feat: Adding new constants for roles

---
+++ Library/Library.Application/Constants/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Application.Constants;
+
+public static class Roles
+{
+    public const string USER = "USER";
+    public const string LIBRARIAN = "LIBRARIAN";
+    public const string AUTHOR = "AUTHOR";
+}
