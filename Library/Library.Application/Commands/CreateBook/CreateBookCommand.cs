902055b1a33da24c1c0a472995f8faf333d87122
fabio <fabioamorin120@gmail.com>
2023-08-17 20:22:45 -0300

    feat: Adding new command and handler to project

---
+++ Library/Library.Application/Commands/CreateBook/CreateBookCommand.cs
@@ -0,0 +1,28 @@
+﻿namespace Library.Application.Commands.CreateBook;
+
+public struct CreateBookCommand : IRequest<ResultViewModel<Guid>>
+{
+    public string Title { get; init; }
+    public DateTime Year { get; init; } 
+    public int Pages { get; init; }
+    public Guid AuthorId { get; init; }
+    public Guid LibraryId { get; init; }
+    public BookGenre Genre { get; init; }
+
+    public CreateBookCommand(
+        string title, 
+        DateTime year, 
+        int pages, 
+        Guid authorId, 
+        Guid libraryId, 
+        BookGenre genre
+    )
+    {
+        Title = title;
+        Year = year;
+        Pages = pages;
+        AuthorId = authorId;
+        LibraryId = libraryId;
+        Genre = genre;
+    }
+}
---
+++ Library/Library.Application/Commands/CreateBook/CreateBookCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using Library.Infrastructure;
+
+namespace Library.Application.Commands.CreateBook;
+
+public class CreateBookCommandHandler : IRequestHandler<CreateBookCommand, ResultViewModel<Guid>>
+{
+    public readonly LibraryContext _context;
+
+    public CreateBookCommandHandler(LibraryContext context)
+        => _context = context;
+    
+    public Task<ResultViewModel<Guid>> Handle(CreateBookCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
