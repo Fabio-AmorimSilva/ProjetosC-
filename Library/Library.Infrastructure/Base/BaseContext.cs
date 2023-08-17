36169c978189e069d308c3ed66df65ba41a6a37e
fabio <fabioamorin120@gmail.com>
2023-08-17 20:18:39 -0300

    feat: Adding basecontext

---
+++ Library/Library.Infrastructure/Base/BaseContext.cs
@@ -0,0 +1,41 @@
+﻿namespace Library.Infrastructure.Base;
+
+public class BaseContext : DbContext
+{
+    public BaseContext(DbContextOptions<BaseContext> options): base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){ }
+    
+    public void SetCreateInfo()
+    {
+        if (ChangeTracker.Entries().Any(e => e.State == EntityState.Added))
+        {
+            var createdEntities = ChangeTracker
+                .Entries<BaseEntity>()
+                .Where(e => e.State == EntityState.Added);
+
+            foreach (var entity in createdEntities)
+                entity.Entity.CreatedAt = DateTime.Now;
+        }
+    }
+    
+    public void SetUpdateInfo()
+    {
+        if (ChangeTracker.Entries().Any(e => e.State == EntityState.Modified))
+        {
+            var modifiedEntities = ChangeTracker
+                .Entries<BaseEntity>()
+                .Where(e => e.State == EntityState.Modified);
+
+            foreach (var entity in modifiedEntities)
+                entity.Entity.UpdatedAt = DateTime.Now;
+        }
+    }
+    
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        SetCreateInfo();
+        SetUpdateInfo();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
