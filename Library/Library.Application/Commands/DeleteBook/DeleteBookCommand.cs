c30e189816d5a7b34dab7d08169b1894a740af1c
fabio <fabioamorin120@gmail.com>
2023-08-20 15:07:50 -0300

    feat: Adding delete command and handler

---
+++ Library/Library.Application/Commands/DeleteBook/DeleteBookCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.Application.Commands;
+
+public class DeleteBookCommand : IRequest<ResultViewModel<Unit>>
+{
+    public Guid Id { get; set; }
+
+    public DeleteBookCommand(Guid id)
+        =>  Id = id;
+}
---
+++ Library/Library.Application/Commands/DeleteBook/DeleteBookCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Library.Application.Commands.DeleteBook;
+
+public class DeleteBookCommandHandler : IRequestHandler<DeleteBookCommand, ResultViewModel<Unit>>
+{
+    private readonly LibraryContext _context;
+
+    public DeleteBookCommandHandler(LibraryContext context)
+        =>  _context = context;
+    
+    public async Task<ResultViewModel<Unit>> Handle(DeleteBookCommand request, CancellationToken cancellationToken)
+    {
+        var bookExists = await _context
+            .Books
+            .AnyAsync(b => b.Id == request.Id, cancellationToken);
+
+        if (!bookExists)
+            return new ResultViewModel<Unit>("Book not found");
+
+        _context.Books.Remove(new Book{ Id = request.Id });
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return new ResultViewModel<Unit>("Book has been deleted");
+    }
+}
