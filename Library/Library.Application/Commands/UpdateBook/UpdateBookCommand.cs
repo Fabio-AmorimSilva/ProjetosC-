2755c6bb359763e9a723bb95c41ffee887d374db
fabio <fabioamorin120@gmail.com>
2023-08-20 15:08:18 -0300

    feat: Adding update command and handler

---
+++ Library/Library.Application/Commands/UpdateBook/UpdateBookCommand.cs
@@ -0,0 +1,31 @@
+﻿namespace Library.Application.Commands;
+
+public class UpdateBookCommand : IRequest<ResultViewModel<Unit>>
+{
+    public Guid Id { get; set; }
+    public string Title { get; init; }
+    public DateTime Year { get; init; } 
+    public int Pages { get; init; }
+    public Guid AuthorId { get; init; }
+    public Guid LibraryId { get; init; }
+    public BookGenre Genre { get; init; }
+
+    public UpdateBookCommand(
+        Guid id, 
+        string title, 
+        DateTime year, 
+        int pages, 
+        Guid authorId, 
+        Guid libraryId, 
+        BookGenre genre
+    )
+    {
+        Id = id;
+        Title = title;
+        Year = year;
+        Pages = pages;
+        AuthorId = authorId;
+        LibraryId = libraryId;
+        Genre = genre;
+    }
+}
---
+++ Library/Library.Application/Commands/UpdateBook/UpdateBookCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace Library.Application.Commands.UpdateBook;
+
+public class UpdateBookCommandHandler : IRequestHandler<UpdateBookCommand, ResultViewModel<Unit>>
+{
+    private readonly LibraryContext _context;
+
+    public UpdateBookCommandHandler(LibraryContext context)
+        =>  _context = context;
+    
+    public async Task<ResultViewModel<Unit>> Handle(UpdateBookCommand request, CancellationToken cancellationToken)
+    {
+        var bookFromDatabase = await _context
+            .Books
+            .FirstOrDefaultAsync(b => b.Id == request.Id, cancellationToken);
+
+        if (bookFromDatabase is null)
+            return new ResultViewModel<Unit>("Book not found.");
+        
+        bookFromDatabase.UpdateBook(
+            title: request.Title, 
+            year: request.Year, 
+            pages: request.Pages,
+            authorId: request.AuthorId, 
+            libraryId: request.LibraryId, 
+            genre: request.Genre
+        );
+        
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return new ResultViewModel<Unit>("Book has been updated!");
+    }
+}
