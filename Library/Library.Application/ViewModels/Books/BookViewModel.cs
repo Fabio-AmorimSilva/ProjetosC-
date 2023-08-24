38583dbac6666cd175d6d1f32f8d0ff3abd79e80
fabio <fabioamorin120@gmail.com>
2023-08-24 20:12:46 -0300

    feat: Adding new viewmodel

---
+++ Library/Library.Application/ViewModels/Books/BookViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Application.ViewModels.Books;
+
+public readonly record struct BookViewModel
+{
+    public required string Title { get; init; }
+    public required DateTime Year { get; init; }
+    public required int Pages { get; init; }
+    public required int Quantity { get; init; }
+    public required BookGenre Genre { get; init; }
+}
