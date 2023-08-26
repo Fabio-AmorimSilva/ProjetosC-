0f4e59cfb5a8b69d84fbcc4f1587b2a791388e1d
fabio <fabioamorin120@gmail.com>
2023-08-26 16:29:51 -0300

    feat: Adding UpdateLibraryCommand and Handler

---
+++ Library/Library.Application/Commands/Library/UpdateLibrary/UpdateLibraryCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Library.Application.Commands;
+
+public struct UpdateLibraryCommand : IRequest<ResultViewModel<Unit>>
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public string City { get; init; }
+
+    public UpdateLibraryCommand(Guid id, string name, string city)
+    {
+        Id = id;
+        Name = name;
+        City = city;
+    }
+}
---
+++ Library/Library.Application/Commands/Library/UpdateLibrary/UpdateLibraryCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Library.Application.Commands;
+
+public class UpdateLibraryCommandHandler : IRequestHandler<UpdateLibraryCommand, ResultViewModel<Unit>>
+{
+    private readonly LibraryContext _context;
+
+    public UpdateLibraryCommandHandler(LibraryContext context)
+        => _context = context;
+    
+    public async Task<ResultViewModel<Unit>> Handle(UpdateLibraryCommand request, CancellationToken cancellationToken)
+    {
+        var library = await _context.Libraries.FirstOrDefaultAsync(l => l.Id == request.Id, cancellationToken);
+        if (library is null)
+            return new ResultViewModel<Unit>("Book not found");
+
+        library.UpdateLibrary(request.Name, request.City);
+
+        await _context.SaveChangesAsync(cancellationToken);
+        return new ResultViewModel<Unit>();
+    }
+}
