5ec89fa10b6af3d3b752099e08ddcf6c5fb14862
fabio <fabioamorin120@gmail.com>
2023-08-26 16:30:22 -0300

    feat: Adding CreateLibraryCommand and handler

---
+++ Library/Library.Application/Commands/Library/CreateLibrary/CreateLibraryCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.Application.Commands;
+
+public struct CreateLibraryCommand : IRequest<ResultViewModel<Unit>>
+{
+    public string Name { get; init; }
+    public string City { get; init; }
+
+    public CreateLibraryCommand(string name, string city)
+    {
+        Name = name;
+        City = city;
+    }
+}
---
+++ Library/Library.Application/Commands/Library/CreateLibrary/CreateLibraryCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Library.Application.Commands;
+
+public class CreateLibraryCommandHandler : IRequestHandler<CreateLibraryCommand, ResultViewModel<Unit>>
+{
+    private readonly LibraryContext _context;
+
+    public CreateLibraryCommandHandler(LibraryContext context)
+        => _context = context;
+    
+    public async Task<ResultViewModel<Unit>> Handle(CreateLibraryCommand request, CancellationToken cancellationToken)
+    {
+        var library = new LibraryUnit(
+            name: request.Name,
+            city: request.City
+        );
+
+        await _context.Libraries.AddAsync(library, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return new ResultViewModel<Unit>();
+    }
+}
