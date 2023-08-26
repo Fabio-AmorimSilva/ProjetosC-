15a27d14e750b0a39e6a47df6a7864a9c09107e9
fabio <fabioamorin120@gmail.com>
2023-08-26 16:31:39 -0300

    feat: Adding GetBookQuery and handler

---
+++ Library/Library.Application/Queries/Books/GetBook/GetBookQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.Application.Queries;
+
+public struct GetBookQuery : IRequest<ResultViewModel<BookViewModel>>
+{
+    public Guid Id { get; init; }
+
+    public GetBookQuery(Guid id)
+        =>  Id = id;
+}
---
+++ Library/Library.Application/Queries/Books/GetBook/GetBookQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Library.Application.Queries;
+
+public class GetBookQueryHandler : IRequestHandler<GetBookQuery , ResultViewModel<BookViewModel>>
+{
+    private readonly LibraryContext _context;
+
+    public GetBookQueryHandler(LibraryContext context)
+        => _context = context;
+    
+    public async Task<ResultViewModel<BookViewModel>> Handle(GetBookQuery request, CancellationToken cancellationToken)
+    {
+        var book = await _context.Books.AsNoTracking().FirstOrDefaultAsync(b => b.Id == request.Id, cancellationToken);
+        if (book is null)
+            return new ResultViewModel<BookViewModel>("Book not found");
+
+        return new ResultViewModel<BookViewModel>(new BookViewModel
+        {
+            Title = book.Title,
+            Genre = book.Genre,
+            Quantity = book.Quantity,
+            Pages = book.Pages,
+            Year = book.Year
+        });
+    }
+}
