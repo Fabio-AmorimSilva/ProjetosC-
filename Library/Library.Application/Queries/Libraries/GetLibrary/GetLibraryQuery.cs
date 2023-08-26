b6a4e472c2c5ada1b2465125623603599be50f11
fabio <fabioamorin120@gmail.com>
2023-08-26 16:32:07 -0300

    feat: Adding GetLibraryQuery and handler

---
+++ Library/Library.Application/Queries/Libraries/GetLibrary/GetLibraryQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.Application.Queries;
+
+public struct GetLibraryQuery : IRequest<ResultViewModel<LibraryUnitViewModel>>
+{
+    public Guid Id { get; init; }
+
+    public GetLibraryQuery(Guid id)
+        => Id = id;
+}
---
+++ Library/Library.Application/Queries/Libraries/GetLibrary/GetLibraryQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Library.Application.Queries;
+
+public class GetLibraryQueryHandler : IRequestHandler<GetLibraryQuery, ResultViewModel<LibraryUnitViewModel>>
+{
+    private readonly LibraryContext _context;
+
+    public GetLibraryQueryHandler(LibraryContext context)
+        => _context = context;
+    
+    public async Task<ResultViewModel<LibraryUnitViewModel>> Handle(GetLibraryQuery request, CancellationToken cancellationToken)
+    {
+        var library = await _context.Libraries
+            .AsNoTrackingWithIdentityResolution()
+            .FirstOrDefaultAsync(l => l.Id == request.Id, cancellationToken);
+
+        if (library is null)
+            return new ResultViewModel<LibraryUnitViewModel>("Library not found");
+
+        return new ResultViewModel<LibraryUnitViewModel>(new LibraryUnitViewModel
+        {
+            Name = library.Name,
+            City = library.City
+        });
+    }
+}
