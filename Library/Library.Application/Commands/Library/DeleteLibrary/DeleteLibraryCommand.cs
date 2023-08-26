7887d9cb2b87c001a39efb105c61914cd8383a46
fabio <fabioamorin120@gmail.com>
2023-08-26 16:30:44 -0300

    feat: Adding DeleteLibraryCommand and handler

---
+++ Library/Library.Application/Commands/Library/DeleteLibrary/DeleteLibraryCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.Application.Commands;
+
+public struct DeleteLibraryCommand : IRequest<ResultViewModel<Unit>>
+{
+    public Guid Id { get; init; }
+
+    public DeleteLibraryCommand(Guid id)
+        => Id = id;
+}
---
+++ Library/Library.Application/Commands/Library/DeleteLibrary/DeleteLibraryCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Library.Application.Commands;
+
+public class DeleteLibraryCommandHandler : IRequestHandler<DeleteLibraryCommand, ResultViewModel<Unit>>
+{
+    private readonly LibraryContext _context;
+
+    public DeleteLibraryCommandHandler(LibraryContext context)
+        => _context = context;
+        
+    public async Task<ResultViewModel<Unit>> Handle(DeleteLibraryCommand request, CancellationToken cancellationToken)
+    {
+        var library = await _context.Libraries
+            .FirstOrDefaultAsync(l => l.Id == request.Id, cancellationToken);
+
+        if (library is null)
+            return new ResultViewModel<Unit>("Library not found");
+
+        _context.Libraries.Remove(library);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return new ResultViewModel<Unit>();
+    }
+}
