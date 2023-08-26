45f1b2eaab4760a7bfbad94aedbc29f50e77b2a0
fabio <fabioamorin120@gmail.com>
2023-08-26 16:33:32 -0300

    refactor: Changing controller to use commands

--- Library/Library.WebApi/Controllers/BookController.cs
+++ Library/Library.WebApi/Controllers/BookController.cs
@@ -1,54 +1,44 @@
-﻿using Library.Application.Queries;
-
-namespace Library.WebApi.Controllers;
+﻿namespace Library.WebApi.Controllers;
 
 [ApiController]
 [Authorize]
+[Route("v1/books")]
 public class BookController : ControllerBase
 {
     private readonly IMediator _mediator;
-    private readonly LibraryContext _context;
-
-    public BookController(
-        LibraryContext context,
-        IMediator mediator)
-    {
-        _context = context;
-        _mediator = mediator;
-    }
-
-    [HttpGet("v1/books")]
+    
+    public BookController(IMediator mediator)
+        =>  _mediator = mediator;
+    
+    [HttpGet]
     public async Task<ActionResult<ResultViewModel<IEnumerable<Book>>>> Get()
     {
         var result = await _mediator.Send(new ListBooksQuery(), cancellationToken: default);
         return Ok(result);
     }
 
-    [HttpGet("v1/books/{id:guid}")]
+    [HttpGet("{id:guid}")]
     public async Task<IActionResult> GetById([FromRoute] Guid id)
     {
-        var book = await _context.Books.FirstOrDefaultAsync(b => b.Id == id, cancellationToken: default);
-        if (book is null)
-            return NotFound(new ResultViewModel<Book>("Book not found"));
-
-        return Ok(new ResultViewModel<Book>(book));
+        var result = await _mediator.Send(new GetBookQuery(id));
+        return Ok((result));
     }
 
-    [HttpPost("v1/books")]
+    [HttpPost]
     public async Task<ActionResult<Guid>> Post([FromBody] CreateBookCommand command)
     {
         var result = await _mediator.Send(command);
         return Created($"{result}", result.Data);
     }
 
-    [HttpPut("v1/books/{id:guid}")]
+    [HttpPut("{id:guid}")]
     public async Task<ActionResult> Put(Guid id, [FromBody] UpdateBookCommand command)
     {
         await _mediator.Send(command);
         return NoContent();
     }
 
-    [HttpDelete("v1/books/{id:guid}")]
+    [HttpDelete("{id:guid}")]
     public async Task<IActionResult> Delete([FromRoute] Guid id)
     {
         var command = new DeleteBookCommand(id);
