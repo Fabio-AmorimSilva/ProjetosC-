1318f8dff5a324551ac084c028b773a68bac5e2c
fabio <fabioamorin120@gmail.com>
2023-08-27 16:02:02 -0300

    refactor: Changing controller to use mediador

--- Library/Library.WebApi/Controllers/AuthorController.cs
+++ Library/Library.WebApi/Controllers/AuthorController.cs
@@ -2,92 +2,47 @@
 
 [ApiController]
 [Authorize]
+[Route("v1/author")]
 public class AuthorController : ControllerBase
 {
-    private readonly LibraryContext _context;
-
-    public AuthorController(LibraryContext context)
-        => _context = context;
+    private readonly IMediator _mediator;
     
-    [HttpGet("v1/author")]
-    public async Task<ActionResult<ResultViewModel<IEnumerable<Author>>>> Get() 
-    { 
-        var authors = await _context
-            .Authors
-            .Include(a => a.Books)
-            .ToListAsync(cancellationToken: default);
-
-        if (authors is null)
-            return NotFound(new ResultViewModel<IEnumerable<Author>>("No authors found"));
-
-        return Ok(new ResultViewModel<IEnumerable<Author>>(authors)); 
+    public AuthorController(IMediator mediator)
+        =>  _mediator = mediator;
+    
+    [HttpGet]
+    public async Task<ActionResult<ResultViewModel<IEnumerable<Author>>>> Get()
+    {
+        var result = await _mediator.Send(new ListAuthorsQuery());
+        return Ok(result); 
     }
 
-    [HttpGet("v1/author/{id:guid}")]
-    public async Task<ActionResult<ResultViewModel<Author>>> GetById([FromRoute] Guid id)
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<ResultViewModel<Author>>> GetById(Guid id)
     {
-        var author = await _context.Authors.FirstOrDefaultAsync(a => a.Id == id, cancellationToken: default);
-        if (author is null)
-            return NotFound(new ResultViewModel<Author>("Author not found"));
-
-        return Ok(new ResultViewModel<Author>(author));
+        var result = await _mediator.Send(new GetAuthorQuery(id));
+        return Ok(result);
     }
 
-
-    [HttpPost("v1/author")]
-    public async Task<ActionResult<Guid>> Post([FromBody] AuthorRequestViewModel author)
+    [HttpPost]
+    public async Task<ActionResult<Guid>> Post([FromBody] CreateAuthorCommand command)
     {
-        var authorFromDatabase = await _context
-            .Authors
-            .FirstOrDefaultAsync(a => a.Name == author.Name, cancellationToken: default);
-
-        if (authorFromDatabase is not null)
-            return NotFound(new ResultViewModel<Author>("Author already exists. Please enter another one"));
-
-        var newAuthor = new Author(
-            name: author.Name,
-            country: author.Country,
-            birth: author.Birth);
-
-        await _context.Authors.AddAsync(newAuthor);
-        await _context.SaveChangesAsync();
-
-        return Created($"{newAuthor.Name}", author);
+        var result = await _mediator.Send(command);
+        return Created($"{result.Data}", result);
     }
 
-    [HttpPut("v1/author/{id:guid}")]
-    public async Task<ActionResult> Put([FromBody] AuthorRequestViewModel author, [FromRoute] Guid id)
+    [HttpPut("{id:guid}")]
+    public async Task<ActionResult> Put(Guid id, [FromBody] UpdateAuthorCommand command)
     {
-        var authorFromDatabase = await _context
-            .Authors
-            .FirstOrDefaultAsync(a => a.Id == id, cancellationToken: default);
-
-        if (authorFromDatabase is null)
-            return NotFound(new ResultViewModel<Author>("Author not found"));
-
-        authorFromDatabase.Name = author.Name;
-        authorFromDatabase.Country = author.Country;
-        authorFromDatabase.Birth = author.Birth;
-
-        _context.Authors.Update(authorFromDatabase);
-        await _context.SaveChangesAsync();
-
+        await _mediator.Send(command);
         return NoContent();
     }
 
-    [HttpDelete("v1/author/{id:guid}")]
-    public async Task<ActionResult> Delete([FromRoute] Guid id)
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> Delete(Guid id)
     {
-        var authorFromDatabase = await _context
-            .Authors
-            .FirstOrDefaultAsync(a => a.Id == id, cancellationToken: default);
-
-        if(authorFromDatabase is null)
-            return NotFound(new ResultViewModel<Author>("Author not found"));
-
-        _context.Authors.Remove(authorFromDatabase);
-        await _context.SaveChangesAsync();
-
+        var command = new DeleteAuthorCommand(id);
+        await _mediator.Send(command);
         return NoContent();
     }
 }
