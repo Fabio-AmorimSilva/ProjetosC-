5995a90f2b2a18abeab43ff82e9217c5f686eb75
fabio <fabioamorin120@gmail.com>
2023-08-27 15:59:27 -0300

    feat: Adding DeleteAuthorCommand and handler

---
+++ Library/Library.Application/Commands/Author/DeleteAuthor/DeleteAuthorCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.Application.Commands;
+
+public struct DeleteAuthorCommand : IRequest<ResultViewModel<Unit>>
+{
+    public Guid Id { get; init; }
+
+    public DeleteAuthorCommand(Guid id)
+        => Id = id;
+}
