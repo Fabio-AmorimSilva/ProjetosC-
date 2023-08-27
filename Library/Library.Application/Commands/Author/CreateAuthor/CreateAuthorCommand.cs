e9b5784f93fe4f4f6edd35b1e08cc39e13813093
fabio <fabioamorin120@gmail.com>
2023-08-27 15:59:10 -0300

    feat: Adding CreateAuthorCommand and handler

---
+++ Library/Library.Application/Commands/Author/CreateAuthor/CreateAuthorCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Library.Application.Commands;
+
+public class CreateAuthorCommand : IRequest<ResultViewModel<Unit>>
+{
+    public string Name { get; init; }
+    public string Country { get; init; }
+    public DateTime Birth { get; init; }
+
+    public CreateAuthorCommand(string name, string country, DateTime birth)
+    {
+        Name = name;
+        Country = country;
+        Birth = birth;
+    }
+}
---
+++ Library/Library.Application/Commands/Author/CreateAuthor/CreateAuthorCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Library.Application.Commands;
+
+public class CreateAuthorCommandHandler : IRequestHandler<CreateAuthorCommand, ResultViewModel<Unit>>
+{
+    private readonly LibraryContext _context;
+
+    public CreateAuthorCommandHandler(LibraryContext context)
+        => _context = context;
+    
+    public async Task<ResultViewModel<Unit>> Handle(CreateAuthorCommand request, CancellationToken cancellationToken)
+    {
+        var author = new Author(
+            name: request.Name,
+            country: request.Country,
+            birth: request.Birth
+        );
+
+        await _context.Authors.AddAsync(author, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return new ResultViewModel<Unit>();
+    }
+}
