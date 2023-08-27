39d4b0b976e9443459fe7ee8ab5fa657b8029665
fabio <fabioamorin120@gmail.com>
2023-08-27 16:02:44 -0300

    refactor: Updating books to be nullable

--- Library/Library.Application/ViewModels/Authors/AuthorViewModel.cs
+++ Library/Library.Application/ViewModels/Authors/AuthorViewModel.cs
@@ -5,5 +5,5 @@ public readonly record struct AuthorViewModel
     public required string Name { get; init; }
     public required string Country { get; init; }
     public required DateTime Birth { get; init; }
-    public IEnumerable<Book> Books { get; init; }
+    public IEnumerable<Book>? Books { get; init; }
 }
