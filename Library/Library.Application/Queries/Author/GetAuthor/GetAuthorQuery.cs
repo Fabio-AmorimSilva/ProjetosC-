006e405d8ff8a181b7e49e1b0cf59ebf07cf6646
fabio <fabioamorin120@gmail.com>
2023-08-27 16:00:57 -0300

    feat: Adding new GetAuthorQuery and handler

---
+++ Library/Library.Application/Queries/Author/GetAuthor/GetAuthorQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.Application.Queries;
+
+public struct GetAuthorQuery : IRequest<ResultViewModel<AuthorViewModel>>
+{
+    public Guid Id { get; init; }
+
+    public GetAuthorQuery(Guid id)
+        => Id = id;
+}
---
+++ Library/Library.Application/Queries/Author/GetAuthor/GetAuthorQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Library.Application.Queries;
+
+public class GetAuthorQueryHandler : IRequestHandler<GetAuthorQuery, ResultViewModel<AuthorViewModel>>
+{
+    private readonly LibraryContext _context;
+
+    public GetAuthorQueryHandler(LibraryContext context)
+        => _context = context;
+    
+    public async Task<ResultViewModel<AuthorViewModel>> Handle(GetAuthorQuery request, CancellationToken cancellationToken)
+    {
+        var author = await _context.Authors
+            .AsNoTrackingWithIdentityResolution()
+            .FirstOrDefaultAsync(a => a.Id == request.Id, cancellationToken);
+
+        if (author is null)
+            return new ResultViewModel<AuthorViewModel>("Author not found");
+
+        return new ResultViewModel<AuthorViewModel>(new AuthorViewModel
+        {
+            Name = author.Name,
+            Country = author.Country,
+            Birth = author.Birth
+        });
+    }
+}
