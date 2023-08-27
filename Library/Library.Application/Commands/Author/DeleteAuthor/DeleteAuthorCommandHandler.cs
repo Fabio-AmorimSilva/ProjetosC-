9ceb7f51b25730c212d415a5a5f94d5cbbee09a5
fabio <fabioamorin120@gmail.com>
2023-08-27 16:01:41 -0300

    feat: Adding DeleteAuthorCommandHandler

---
+++ Library/Library.Application/Commands/Author/DeleteAuthor/DeleteAuthorCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Library.Application.Commands;
+
+public class DeleteAuthorCommandHandler : IRequestHandler<DeleteAuthorCommand, ResultViewModel<Unit>>
+{
+    private readonly LibraryContext _context;
+
+    public DeleteAuthorCommandHandler(LibraryContext context)
+        => _context = context;
+    
+    public async Task<ResultViewModel<Unit>> Handle(DeleteAuthorCommand request, CancellationToken cancellationToken)
+    {
+        var author = await _context.Authors.FirstOrDefaultAsync(a => a.Id == request.Id, cancellationToken);
+        if (author is null)
+            return new ResultViewModel<Unit>("Author not found");
+
+        _context.Authors.Remove(author);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return new ResultViewModel<Unit>();
+    }
+}
