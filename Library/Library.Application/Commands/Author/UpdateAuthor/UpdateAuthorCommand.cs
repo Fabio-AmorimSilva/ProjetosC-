d8b774f39f6bbde7f3092a20499f06ce0b667232
fabio <fabioamorin120@gmail.com>
2023-08-27 15:59:45 -0300

    feat: Adding UpdateAuthorCommand and handler

---
+++ Library/Library.Application/Commands/Author/UpdateAuthor/UpdateAuthorCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace Library.Application.Commands;
+
+public struct UpdateAuthorCommand : IRequest<ResultViewModel<Unit>>
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public string Country { get; init; }
+    public DateTime Birth { get; init; }
+
+    public UpdateAuthorCommand(Guid id, string name, string country, DateTime birth)
+    {
+        Id = id;
+        Name = name;
+        Country = country;
+        Birth = birth;
+    }
+}
---
+++ Library/Library.Application/Commands/Author/UpdateAuthor/UpdateAuthorCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Library.Application.Commands;
+
+public class UpdateAuthorCommandHandler : IRequestHandler<UpdateAuthorCommand, ResultViewModel<Unit>>
+{
+    private readonly LibraryContext _context;
+
+    public UpdateAuthorCommandHandler(LibraryContext context)
+        =>  _context = context;
+    
+    public async Task<ResultViewModel<Unit>> Handle(UpdateAuthorCommand request, CancellationToken cancellationToken)
+    {
+        var author = await _context.Authors.FirstOrDefaultAsync(a => a.Id == request.Id, cancellationToken);
+        if (author is null)
+            return new ResultViewModel<Unit>("Author not found");
+        
+        author.UpdateAuthor(
+            name: request.Name,
+            country: author.Country,
+            birth: author.Birth
+        );
+
+        await _context.SaveChangesAsync(cancellationToken);
+        
+        return new ResultViewModel<Unit>();
+    }
+}
