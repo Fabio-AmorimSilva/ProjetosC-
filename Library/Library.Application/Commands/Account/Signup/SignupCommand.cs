05610da07581b8e9d46c72cb54c9c54db732b9f7
fabio <fabioamorin120@gmail.com>
2023-08-30 20:30:45 -0300

    feat: Adding SignupCommand and handler

---
+++ Library/Library.Application/Commands/Account/Signup/SignupCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace Library.Application.Commands;
+
+public struct SignupCommand : IRequest<ResultViewModel<Unit>>
+{
+    public string Name { get; init; }
+    public string Email { get; init; }
+    public string Password { get; init; }
+    public Role Role { get; init; }
+
+    public SignupCommand(
+        string name, 
+        string email, 
+        string password, 
+        Role role
+    )
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+        Role = role;
+    }
+}
---
+++ Library/Library.Application/Commands/Account/Signup/SignupCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace Library.Application.Commands;
+
+public class SignupCommandHandler : IRequestHandler<SignupCommand, ResultViewModel<Unit>>
+{
+    private readonly LibraryContext _context;
+
+    public SignupCommandHandler(LibraryContext context)
+        => _context = context;
+    
+    public async Task<ResultViewModel<Unit>> Handle(SignupCommand request, CancellationToken cancellationToken)
+    {
+        var userExists = await _context
+            .Users
+            .AnyAsync(u => u.Email == request.Email, cancellationToken);
+
+        if(userExists)
+            return new ResultViewModel<Unit>("Email already in use.");
+        
+        var user = new User(
+            name: request.Name,
+            email: request.Email,
+            password: request.Password,
+            role: request.Role);
+
+        await _context.Users.AddAsync(user, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return new ResultViewModel<Unit>();
+    }
+}
