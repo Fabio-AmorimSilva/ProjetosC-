5d81660bf6c44545bb8bdc42621f9eddc5429e1e
fabio <fabioamorin120@gmail.com>
2023-08-30 20:30:23 -0300

    feat: Adding LoginCommand and handler

---
+++ Library/Library.Application/Commands/Account/Login/LoginCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace Library.Application.Commands;
+
+public struct LoginCommand : IRequest<string>
+{
+    public string Username { get; init; }
+    public string Password { get; init; }
+
+    public LoginCommand(
+        string username, 
+        string password
+    )
+    {
+        Username = username;
+        Password = password;
+    }
+}
---
+++ Library/Library.Application/Commands/Account/Login/LoginCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Library.Application.Commands;
+
+public class LoginCommandHandler : IRequestHandler<LoginCommand, string>
+{
+    private readonly LibraryContext _context;
+    private readonly TokenService _tokenService;
+    
+    public LoginCommandHandler(
+        LibraryContext context, 
+        TokenService tokenService
+    )
+    {
+        _context = context;
+        _tokenService = tokenService;
+    }
+
+    public async Task<string> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _context.Users
+            .FirstOrDefaultAsync(u => u.Email == request.Username, cancellationToken);
+
+        if (user is null)
+            return "User not found";
+
+        var token = _tokenService.GenerateToken(user);
+
+        return token;
+    }
+}
