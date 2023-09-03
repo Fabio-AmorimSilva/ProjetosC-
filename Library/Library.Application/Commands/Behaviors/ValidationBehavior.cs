e4d6f867d6b7d3aa1082aa63bb968a8c30d464b6
fabio <fabioamorin120@gmail.com>
2023-09-03 16:41:25 -0300

    feat: Adding class to fail fast validation

---
+++ Library/Library.Application/Commands/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,18 @@
+﻿public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly IValidator<TRequest> _validator;
+
+    public ValidationBehavior(IValidator<TRequest> validator)
+        =>  _validator = validator;
+    
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+        
+        return await next();
+    }
+}
