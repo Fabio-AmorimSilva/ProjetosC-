090d77b80da5a80ee9aa56de99080d0493170b05
fabio <fabioamorin120@gmail.com>
2023-09-04 19:54:22 -0300

    chore: Adding new namespaces to global usings

--- Library/Library.Application/GlobalUsings.cs
+++ Library/Library.Application/GlobalUsings.cs
@@ -3,6 +3,7 @@
 global using System.IdentityModel.Tokens.Jwt;
 global using System.Security.Claims;
 global using System.Text;
+global using System.Text.Json;
 global using FluentValidation;
 global using Library.Application.Extensions;
 global using Library.Application.Options;
