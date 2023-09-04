70cf0f29c1c0dd088891b087533b67173f26a562
fabio <fabioamorin120@gmail.com>
2023-09-04 19:54:40 -0300

    feat: Creating new ExceptionFilter

---
+++ Library/Library.WebApi/Filters/Exceptions/ExceptionFilter.cs
@@ -0,0 +1,18 @@
+﻿namespace Library.WebApi.Filters.Exceptions;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        var exception = context.Exception;
+        if (exception.GetType() == typeof(ValidationException))
+        {
+            var error = new Error
+            {
+                StatusCode = 422,
+                Message = exception.Message
+            };
+            context.Result = new JsonResult(error);
+        }
+    }
+}
