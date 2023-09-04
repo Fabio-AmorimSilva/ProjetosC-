a2a32ae4240f27625c3513cab5f6be7f4f04dae6
fabio <fabioamorin120@gmail.com>
2023-09-04 19:54:57 -0300

    feat: Creating new error class

---
+++ Library/Library.WebApi/Utils/Error.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.WebApi.Utils;
+
+public class Error
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+
+    public override string ToString()
+        => JsonSerializer.Serialize(this);
+}
