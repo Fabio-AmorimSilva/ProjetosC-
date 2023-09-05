27fd67500873528c966ae500430963c9fde9748d
fabio <fabioamorin120@gmail.com>
2023-09-05 19:46:24 -0300

    feat: Adding new validators for book commands

---
+++ Library/Library.Application/Commands/Book/DeleteBook/DeleteBookCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Application.Commands;
+
+public class DeleteBookCommandValidator : AbstractValidator<DeleteBookCommand>
+{
+    public DeleteBookCommandValidator()
+    {
+        RuleFor(command => command.Id)
+            .NotEmpty()
+            .WithMessage("Id should not be empty");
+    }
+}
---
+++ Library/Library.Application/Commands/Book/UpdateBook/UpdateBookCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿namespace Library.Application.Commands;
+
+public class UpdateBookCommandValidator : AbstractValidator<UpdateBookCommand>
+{
+    public UpdateBookCommandValidator()
+    {
+        RuleFor(command => command.Id)
+            .NotEmpty()
+            .WithMessage("Id should not be empty");
+        
+        RuleFor(command => command.Title)
+            .MaximumLength(80)
+            .WithMessage("Book title be less than 80 characters");
+
+        RuleFor(command => command.Pages)
+            .Must(p => p > 0)
+            .WithMessage("Pages should not be 0");
+    }
+}
