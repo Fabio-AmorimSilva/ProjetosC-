6ed9e56e7a9395a34b2cf880e608c191fa3562eb
fabio <fabioamorin120@gmail.com>
2023-09-05 19:46:52 -0300

    feat: Adding validation for create book command

---
+++ Library/Library.Application/Commands/Book/CreateBook/CreateBookCommandValidator.cs
@@ -0,0 +1,35 @@
+﻿namespace Library.Application.Commands;
+
+public class CreateBookCommandValidator : AbstractValidator<CreateBookCommand>
+{
+    public CreateBookCommandValidator()
+    {
+        RuleFor(command => command.Title)
+            .NotEmpty()
+            .WithMessage("Book title should not be empty");
+        
+        RuleFor(command => command.Title)
+            .MaximumLength(80)
+            .WithMessage("Book title be less than 80 characters");
+
+        RuleFor(command => command.Pages)
+            .Must(p => p > 0)
+            .WithMessage("Pages should not be 0");
+        
+        RuleFor(command => command.Year)
+            .NotEmpty()
+            .WithMessage("Year should not be empty");
+        
+        RuleFor(command => command.AuthorId)
+            .NotEmpty()
+            .WithMessage("Author should not be empty");
+        
+        RuleFor(command => command.LibraryId)
+            .NotEmpty()
+            .WithMessage("Library should not be empty");
+        
+        RuleFor(command => command.Genre)
+            .NotEmpty()
+            .WithMessage("Genre should not be empty");
+    }
+}
