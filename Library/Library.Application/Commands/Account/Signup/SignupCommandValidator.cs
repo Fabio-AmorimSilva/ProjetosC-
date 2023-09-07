aea0f35cb3413413558a11d8d320cfd155ca83e6
fabio <fabioamorin120@gmail.com>
2023-09-07 15:44:49 -0300

    feat: Adding validator for SignupCommand

---
+++ Library/Library.Application/Commands/Account/Signup/SignupCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿namespace Library.Application.Commands;
+
+public class SignupCommandValidator : AbstractValidator<SignupCommand>
+{
+    public SignupCommandValidator()
+    {
+        RuleFor(command => command.Name)
+            .NotEmpty()
+            .WithMessage("Name cannot be empty");
+        
+        RuleFor(command => command.Name)
+            .MaximumLength(80)
+            .WithMessage("Name has to be less than 80 characters");
+        
+        RuleFor(command => command.Password)
+            .NotEmpty()
+            .WithMessage("Password cannot be empty");
+        
+        RuleFor(command => command.Password)
+            .MaximumLength(256)
+            .WithMessage("Password has to be less than 256 characters");
+        
+        RuleFor(command => command.Email)
+            .NotEmpty()
+            .WithMessage("Email cannot be empty");
+        
+        RuleFor(command => command.Role)
+            .NotEmpty()
+            .WithMessage("Role cannot be empty");
+    }
+}
