58eefb3fd84df2339330d247cff655e2c696b694
fabio <fabioamorin120@gmail.com>
2023-09-07 16:31:07 -0300

    feat: Creating new versioning configuration

---
+++ Library/Library.WebApi/Configuration/ConfigureVersioning.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Versioning;
+
+namespace Library.WebApi.Configuration;
+
+public static class ConfigureVersioning
+{
+    public static IServiceCollection AddVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.ReportApiVersions = true;
+            options.ApiVersionReader = ApiVersionReader.Combine(
+                new UrlSegmentApiVersionReader(),
+                new HeaderApiVersionReader("x-api-version"),
+                new MediaTypeApiVersionReader("x-api-version")
+            );
+        });
+
+        services.AddVersionedApiExplorer(setup =>
+        {
+            setup.GroupNameFormat = "'v'VVV";
+            setup.SubstituteApiVersionInUrl = true;
+        });
+
+        return services;
+    }
+}
