d0f234c5151d15b09819804cc2085bfb0cdeeff3
fabio <fabioamorin120@gmail.com>
2023-09-07 16:31:49 -0300

    chore: Adding versioning setup in Program.cs

--- Library/Library.WebApi/Program.cs
+++ Library/Library.WebApi/Program.cs
@@ -11,18 +11,29 @@ ConfigureExceptionFilter(builder);
 
 //External Configs
 builder.Services.AuthenticationConfig(builder.Configuration);
+builder.Services.AddVersioning();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
 
 var app = builder.Build();
+
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions.Reverse())
+        {
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                description.GroupName.ToUpperInvariant());
+        }
+    });
 }
 
 app.UseCors(x => x
