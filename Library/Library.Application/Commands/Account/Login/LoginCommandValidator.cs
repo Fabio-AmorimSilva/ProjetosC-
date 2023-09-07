450d0cc89e85fcc705d92f891f078e570265de6e
fabio <fabioamorin120@gmail.com>
2023-09-07 15:44:33 -0300

    feat: Adding validator for LoginCommand

---
+++ Library/Library.Application/Commands/Account/Login/LoginCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace Library.Application.Commands;
+
+public class LoginCommandValidator : AbstractValidator<LoginCommand>
+{
+    public LoginCommandValidator()
+    {
+        RuleFor(command => command.Username)
+            .NotEmpty()
+            .WithMessage("Username cannot be empty");
+        
+        RuleFor(command => command.Password)
+            .NotEmpty()
+            .WithMessage("Password cannot be empty");
+    }
+}
