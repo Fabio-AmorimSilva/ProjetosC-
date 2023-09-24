ce0e80817de4922a0db45ae9a506046b5fc1063e
fabio <fabioamorin120@gmail.com>
2023-09-24 12:02:34 -0300

    Feat: Adding new extension method for BaseEntity configuration

---
+++ Library/Library.Infrastructure/Extensions/ConfigureEntityConventionsExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Library.Infrastructure.Extensions;
+
+public static class ConfigureEntityConventionsExtensions
+{
+    public static void ConfigureEntityConventions(this EntityTypeBuilder builder)
+    {
+        builder.As<EntityTypeBuilder>().TryConfigureEntityBase();
+    }
+    
+    public static void TryConfigureEntityBase(this EntityTypeBuilder builder)
+    {
+        if (builder.Metadata.ClrType.IsAssignableTo<BaseEntity>())
+            return;
+
+        builder
+            .HasKey(nameof(BaseEntity.Id));
+
+        builder
+            .Property(nameof(BaseEntity.Id))
+            .ValueGeneratedNever();
+        
+        builder
+            .Property(nameof(BaseEntity.CreatedAt))
+            .IsRequired();
+        
+        builder
+            .Property(nameof(BaseEntity.Id))
+            .IsRequired(false);
+    }
+
+    private static T As<T>(this object obj)
+        => (T)obj;
+}
