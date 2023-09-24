d273f6c04b684ba47536d379294b8d15575582a7
fabio <fabioamorin120@gmail.com>
2023-09-24 12:04:45 -0300

    chore: Adding new namespaces

--- Library/Library.Infrastructure/GlobalUsings.cs
+++ Library/Library.Infrastructure/GlobalUsings.cs
@@ -1,7 +1,9 @@
 ﻿// Global using directives
 
+global using Autofac;
 global using Library.Domain.Entities;
 global using Library.Domain.Entities.Core;
 global using Library.Infrastructure.Base;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Library.Infrastructure.Extensions;
