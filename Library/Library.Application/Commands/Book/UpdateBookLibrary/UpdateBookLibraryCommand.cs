54fa016a06c97a17c790f5258bad771129d7fc5c
fabio <fabioamorin120@gmail.com>
2023-10-01 16:08:02 -0300

    feat: Creating new command and handler for updating book library

---
+++ Library/Library.Application/Commands/Book/UpdateBookLibrary/UpdateBookLibraryCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace Library.Application.Commands;
+
+public struct UpdateBookLibraryCommand : IRequest<ResultViewModel<Unit>>
+{
+    public Guid LibraryId { get; init; }
+    public Guid BookId { get; init; }
+}
---
+++ Library/Library.Application/Commands/Book/UpdateBookLibrary/UpdateBookLibraryCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace Library.Application.Commands;
+
+public class UpdateBookLibraryCommandHandler : IRequestHandler<UpdateBookLibraryCommand, ResultViewModel<Unit>>
+{
+    private readonly LibraryContext _context;
+
+    public UpdateBookLibraryCommandHandler(LibraryContext context)
+        => _context = context;
+    
+    public async Task<ResultViewModel<Unit>> Handle(UpdateBookLibraryCommand request, CancellationToken cancellationToken)
+    {
+        var libraryExists = await _context.Libraries
+            .AnyAsync(lu => lu.Id == request.LibraryId, cancellationToken);
+
+        if (libraryExists is false)
+            return new ResultViewModel<Unit>("Library not found");
+
+        var book = await _context.Books
+            .FirstOrDefaultAsync(b => b.Id == request.BookId, cancellationToken);
+
+        if (book is null)
+            return new ResultViewModel<Unit>("Book not found");
+        
+        book.UpdateLibrary(request.LibraryId);
+
+        return new ResultViewModel<Unit>();
+    }
+}
