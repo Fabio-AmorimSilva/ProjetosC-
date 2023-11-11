17cfc2e878b605cc72c764957a1cca332629ff2f
fabio <fabioamorin120@gmail.com>
2023-11-11 19:04:21 -0300

    chore: Adding new namespaces

--- Library/Library.Domain/GlobalUsings.cs
+++ Library/Library.Domain/GlobalUsings.cs
@@ -4,3 +4,5 @@ global using CommunityToolkit.Diagnostics;
 global using Library.Domain.Entities.Core;
 global using Library.Domain.Entities.Enums;
 global using Library.Domain.Entities.ErrorHandling;
+global using Ardalis.Specification;
+global using Library.Domain.Entities;
