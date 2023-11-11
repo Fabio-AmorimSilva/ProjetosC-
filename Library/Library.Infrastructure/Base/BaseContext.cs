4010e7442bc738e9ad363752ca70770fd92fdc9c
fabio <fabioamorin120@gmail.com>
2023-11-11 16:44:25 -0300

    refactor: Updating validations conditions

--- Library/Library.Infrastructure/Base/BaseContext.cs
+++ Library/Library.Infrastructure/Base/BaseContext.cs
@@ -8,31 +8,31 @@ public class BaseContext : DbContext
 
     private void SetCreateInfo()
     {
-        if (ChangeTracker.Entries().Any(e => e.State == EntityState.Added))
-        {
-            var createdEntities = ChangeTracker
-                .Entries<BaseEntity>()
-                .Where(e => e.State == EntityState.Added);
+        if (ChangeTracker.Entries().All(e => e.State != EntityState.Added)) 
+            return;
+        
+        var createdEntities = ChangeTracker
+            .Entries<BaseEntity>()
+            .Where(e => e.State == EntityState.Added);
 
-            foreach (var entity in createdEntities)
-                entity.Entity.CreatedAt = DateTime.Now;
-        }
+        foreach (var entity in createdEntities)
+            entity.Entity.CreatedAt = DateTime.Now;
     }
 
     private void SetUpdateInfo()
     {
-        if (ChangeTracker.Entries().Any(e => e.State == EntityState.Modified))
-        {
-            var modifiedEntities = ChangeTracker
-                .Entries<BaseEntity>()
-                .Where(e => e.State == EntityState.Modified);
+        if (ChangeTracker.Entries().All(e => e.State != EntityState.Modified)) 
+            return;
+        
+        var modifiedEntities = ChangeTracker
+            .Entries<BaseEntity>()
+            .Where(e => e.State == EntityState.Modified);
 
-            foreach (var entity in modifiedEntities)
-                entity.Entity.UpdatedAt = DateTime.Now;
-        }
+        foreach (var entity in modifiedEntities)
+            entity.Entity.UpdatedAt = DateTime.Now;
     }
     
-    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
     {
         SetCreateInfo();
         SetUpdateInfo();
