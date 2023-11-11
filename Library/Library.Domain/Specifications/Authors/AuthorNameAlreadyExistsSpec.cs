1a83c2c2b3a4ec11e0c9ef0217edbe4e1c538529
fabio <fabioamorin120@gmail.com>
2023-11-11 18:03:10 -0300

    feat: Creating new specification for Author name

---
+++ Library/Library.Domain/Specifications/Authors/AuthorNameAlreadyExistsSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+using Library.Domain.Entities;
+
+namespace Library.Domain.Specifications.Authors;
+
+public sealed class AuthorNameAlreadyExistsSpec : Specification<Author>
+{
+    public AuthorNameAlreadyExistsSpec(Guid id, string name)
+    {
+        Query.Where(a => a.Id != id && a.Name == name);
+    }
+}
