7ba1b0f1d9bd940cc0ad1ba264b0f44dfaa9cae8
fabio <fabioamorin120@gmail.com>
2024-01-21 16:52:06 -0300

    chore: Code formatting

--- Library/Library.Infrastructure/LibraryContext.cs
+++ Library/Library.Infrastructure/LibraryContext.cs
@@ -6,11 +6,11 @@ public class LibraryContext : BaseContext
     public DbSet<Book> Books {  get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<LibraryUnit> Libraries { get; set; }
+    
     public LibraryContext(DbContextOptions<BaseContext> options) : base(options)
     {
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
         => modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
-
 }
