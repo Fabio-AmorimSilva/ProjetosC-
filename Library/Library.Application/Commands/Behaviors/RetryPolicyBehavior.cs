6287144723365921285e3869959ffa5c09b566d0
fabio <fabioamorin120@gmail.com>
2024-02-13 16:11:40 -0300

    feat: Adding new retry routine with library Polly

---
+++ Library/Library.Application/Commands/Behaviors/RetryPolicyBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Library.Application.Utils;
+using Microsoft.Extensions.Logging;
+using Polly;
+
+namespace Library.Application.Commands.Behaviors;
+
+public class RetryPolicyBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger<RetryPolicyBehavior<TRequest, TResponse>> _logger;
+
+    public RetryPolicyBehavior(ILogger<RetryPolicyBehavior<TRequest, TResponse>> logger)
+        => _logger = logger;
+    
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var retry = typeof(TRequest).GetCustomAttribute<RetryPolicyAttribute>();
+        if (retry is null)
+            return await next();
+        
+        return await Policy.Handle<ValidationException>()
+            .WaitAndRetryAsync(
+                retry.RetryCount,
+                i => TimeSpan.FromMilliseconds(i * retry.SleepDuration),
+                (ex, ts, _) => _logger.LogWarning(ex, "Failed to execute handler for request {Request}, retrying after {RetryTimeSpan}s: {ExceptionMessage}", typeof(TRequest).Name, ts.TotalSeconds, ex.Message))
+            .ExecuteAsync(async () => await next());
+    }
+}
---
+++ Library/Library.Application/Utils/RetryPolicyAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Application.Utils;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class RetryPolicyAttribute : Attribute
+{
+    private int _retryCount = 3;
+    private int _sleepDuration = 400;
+
+    public int RetryCount => _retryCount;
+
+    public int SleepDuration => _sleepDuration;
+}
