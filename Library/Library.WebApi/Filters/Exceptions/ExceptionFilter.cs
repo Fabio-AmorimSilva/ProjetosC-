9dcbbd0fd2e513bb82d0703f5b21f3f623d6e0e9
fabio <fabioamorin120@gmail.com>
2024-02-13 16:12:30 -0300

    refactor: Using early if in exception filter

--- Library/Library.WebApi/Filters/Exceptions/ExceptionFilter.cs
+++ Library/Library.WebApi/Filters/Exceptions/ExceptionFilter.cs
@@ -5,14 +5,14 @@ public class ExceptionFilter : IExceptionFilter
     public void OnException(ExceptionContext context)
     {
         var exception = context.Exception;
-        if (exception.GetType() == typeof(ValidationException))
+        if (exception.GetType() != typeof(ValidationException)) 
+            return;
+        
+        var error = new Error
         {
-            var error = new Error
-            {
-                StatusCode = 422,
-                Message = exception.Message
-            };
-            context.Result = new JsonResult(error);
-        }
+            StatusCode = 422,
+            Message = exception.Message
+        };
+        context.Result = new JsonResult(error);
     }
 }
