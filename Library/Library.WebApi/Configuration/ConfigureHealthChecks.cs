b5f4c1be54735b265c58ccb1268b7ee018a17392
fabio <fabioamorin120@gmail.com>
2024-02-22 19:29:38 -0300

    feat: Creating new file for Health Check configurations

---
+++ Library/Library.WebApi/Configuration/ConfigureHealthChecks.cs
@@ -0,0 +1,23 @@
+﻿namespace Library.WebApi.Configuration;
+
+public static class ConfigureHealthChecks
+{
+    public static IServiceCollection AddCustomHealthCheck(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        services.AddHealthChecks()
+            .AddSqlServer(
+                builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty, 
+                name: "Library"
+            );
+
+        builder.Services.AddHealthChecksUI(options =>
+            {
+                options.AddHealthCheckEndpoint("Healthcheck API", "/api/healthcheck");
+                options.SetEvaluationTimeInSeconds(5);
+                options.SetMinimumSecondsBetweenFailureNotifications(5);
+            })
+            .AddInMemoryStorage();
+
+        return services;
+    }
+}
