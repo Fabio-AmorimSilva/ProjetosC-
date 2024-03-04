90d7249d0b74758da10514d326374ff1d080afd7
fabio <fabioamorin120@gmail.com>
2024-03-04 19:31:15 -0300

    feat: Adding new validators

---
+++ Library/Library.Application/Commands/Book/UpdateBookAuthor/UpdateBookAuthorCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using Library.Domain.Messages;
+
+namespace Library.Application.Commands;
+
+public class UpdateBookAuthorCommandValidator : AbstractValidator<UpdateBookAuthorCommand>
+{
+    public UpdateBookAuthorCommandValidator()
+    {
+        RuleFor(command => command.AuthorId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateBookAuthorCommand.AuthorId)));
+        
+        RuleFor(command => command.BookId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateBookAuthorCommand.BookId)));
+    }
+}
---
+++ Library/Library.Application/Commands/Book/UpdateBookLibrary/UpdateBookLibraryCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using Library.Domain.Messages;
+
+namespace Library.Application.Commands;
+
+public class UpdateBookLibraryCommandValidator : AbstractValidator<UpdateBookLibraryCommand>
+{
+    public UpdateBookLibraryCommandValidator()
+    {
+        RuleFor(command => command.LibraryId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateBookLibraryCommand.LibraryId)));
+        
+        RuleFor(command => command.BookId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateBookLibraryCommand.BookId)));
+    }
+}
