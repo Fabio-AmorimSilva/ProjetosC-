e94df1677056811824ad750bfeb5da30b34a46e9
fabio <fabioamorin120@gmail.com>
2024-04-13 16:57:36 -0300

    feat: Adding new responses types

---
+++ Library/Library.Core/Responses/ConflictResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Core.Responses;
+
+public class ConflictResponse<T>: ApiResponse<T>
+{
+    public ConflictResponse(string message)
+    {
+        Errors?.Add(message);
+        StatusCode = 409;
+    }
+}
---
+++ Library/Library.Core/Responses/UnprocessableResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Core.Responses;
+
+public class UnprocessableResponse<T> : ApiResponse<T>
+{
+    public UnprocessableResponse(string message)
+    {
+        Errors?.Add(message);
+        StatusCode = 422;
+    }
+}
