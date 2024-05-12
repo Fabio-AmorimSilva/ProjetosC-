ace95420040d3e34a5d51f60c0fe1ecb6137cc72
fabio <fabioamorin120@gmail.com>
2024-05-12 12:33:00 -0300

    feat: Adding new interceptor for auditable data

---
+++ Library/Library.Infrastructure/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,48 @@
+﻿namespace Library.Infrastructure.Interceptors;
+
+public class AuditableEntityInterceptor : SaveChangesInterceptor
+{
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData, 
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = new()
+    )
+    {
+        SetCreateInfo(eventData.Context);
+        SetUpdateInfo(eventData.Context);
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private void SetCreateInfo(DbContext? context)
+    {
+        if (context is null)
+            return;
+
+        if (context.ChangeTracker.Entries().All(e => e.State != EntityState.Added))
+            return;
+
+        var createdEntities = context.ChangeTracker
+            .Entries<BaseEntity>()
+            .Where(e => e.State == EntityState.Added);
+
+        foreach (var entity in createdEntities)
+            entity.Entity.CreatedAt = DateTime.Now;
+    }
+
+    private void SetUpdateInfo(DbContext? context)
+    {
+        if (context is null)
+            return;
+
+        if (context.ChangeTracker.Entries().All(e => e.State != EntityState.Modified))
+            return;
+
+        var modifiedEntities = context.ChangeTracker
+            .Entries<BaseEntity>()
+            .Where(e => e.State == EntityState.Modified);
+
+        foreach (var entity in modifiedEntities)
+            entity.Entity.UpdatedAt = DateTime.Now;
+    }
+}
