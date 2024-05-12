a719a5d3ecc00aa3eaf6d53e2e6392a38a672c4f
fabio <fabioamorin120@gmail.com>
2024-05-12 12:33:29 -0300

    refactor: Removing unnecessary methods from context

--- Library/Library.Infrastructure/Base/BaseContext.cs
+++ Library/Library.Infrastructure/Base/BaseContext.cs
@@ -1,43 +1,6 @@
-﻿using Library.Core.Entities;
+﻿namespace Library.Infrastructure.Base;
 
-namespace Library.Infrastructure.Base;
-
-public class BaseContext : DbContext
+public class BaseContext(DbContextOptions<BaseContext> options) : DbContext(options)
 {
-    public BaseContext(DbContextOptions<BaseContext> options): base(options) { }
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){ }
-
-    private void SetCreateInfo()
-    {
-        if (ChangeTracker.Entries().All(e => e.State != EntityState.Added)) 
-            return;
-        
-        var createdEntities = ChangeTracker
-            .Entries<BaseEntity>()
-            .Where(e => e.State == EntityState.Added);
-
-        foreach (var entity in createdEntities)
-            entity.Entity.CreatedAt = DateTime.Now;
-    }
-
-    private void SetUpdateInfo()
-    {
-        if (ChangeTracker.Entries().All(e => e.State != EntityState.Modified)) 
-            return;
-        
-        var modifiedEntities = ChangeTracker
-            .Entries<BaseEntity>()
-            .Where(e => e.State == EntityState.Modified);
-
-        foreach (var entity in modifiedEntities)
-            entity.Entity.UpdatedAt = DateTime.Now;
-    }
-    
-    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
-    {
-        SetCreateInfo();
-        SetUpdateInfo();
-        return base.SaveChangesAsync(cancellationToken);
-    }
 }
